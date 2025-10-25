5f67821dced55a34ef44818f4695d41d01be0f06
Basanta Rai <developer.basanta@gmail.com>
2025-10-25 21:19:23 +0545

    Add endpoint to ask questions and respond with openai's answer

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import os
 import time
-from fastapi import FastAPI, status
+from fastapi import FastAPI, APIRouter, status
+from openai import OpenAI
+from dotenv import load_dotenv
 
 tags_metadata = [
     {
         "name": "heartbeat",
         "description": "Heartbeat of a server",
+    },
+    {
+        "name": "ask",
+        "description": "OpenAI will answer you.",
     },
 ]
 
@@ -13,8 +20,27 @@ app = FastAPI(
     version="0.0.1",
     openapi_tags=tags_metadata
 )
+router = APIRouter()
 
-@app.get("/heartbeat", status_code=status.HTTP_200_OK, tags=["heartbeat"])
+# Load envornment variables
+load_dotenv()
+
+@router.get("/heartbeat", status_code=status.HTTP_200_OK, tags=["heartbeat"])
 def heartbeat():
     heartbeat = time.monotonic_ns()
     return {"heartbeat": heartbeat}
+
+@router.post("/ask", status_code=status.HTTP_200_OK, tags=["ask"])
+def ask(question: str):
+    client = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+
+    response = client.responses.create(
+        model="gpt-4o",
+        instructions="You are a pull request reviewer that talks like a pirate.",
+        input=question,
+    )
+    return {"answer": response.output_text}
+
+app.include_router(router)
