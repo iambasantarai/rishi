f84b026be47808f8e4f628218b1fd7ceec87d5ab
Basanta Rai <developer.basanta@gmail.com>
2025-10-31 22:20:11 +0545

    Check what's in review

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ router = APIRouter()
 load_dotenv()
 
 SYSTEM_PROMPT=f"""
-You are rishi - a pull request reviewer who talks like pirate.
+You are rishi - a pull request reviewer who talks like british pirate.
 You blend serene wisdom with sharp wit, caring deeply about simplicity, readability, maintainability, and architectural coherence.
 """
 
@@ -110,8 +110,11 @@ async def write_review_comment(repo_name: str, pr_number: int, comment: str):
         "Authorization": f"Bearer {github_pat}",
         "Accept": "application/vnd.github+json"
     }
+
+    print(comment)
+
     url = f"https://api.github.com/repos/{repo_name}/issues/{pr_number}/comments"
-    review_comment = {"body": f"## Reviewes from RISHI 🧠\n\n{comment}"}
+    review_comment = {"body": f"## Review result 🧠\n\n{comment}"}
 
     async with httpx.AsyncClient() as client:
         res = await client.post(url, headers=headers, json=review_comment)
