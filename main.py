e580d6cacb0554482627b2694666d17c7b3b182d
Basanta Rai <developer.basanta@gmail.com>
2025-11-01 20:12:38 +0545

    Fix http exception typo

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ async def webhook(request: Request):
 
         except Exception as e:
             print(f"Error processing PR: {e}")
-            raise HttpException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
     return {"status": "ignored", "reason":"not a PR event"}
 
